ef4c69bd798374b71eca8e0c3af8b04ac5351fb8
Pedro Marcello Vincenty <vincentypedro@gmail.com>
2024-05-07 10:36:12 -0400

    array practice

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+const destructivelyAppendCat = (name) => {
+    cats.push(name)
+}
+
+const destructivelyPrependCat = (name) => {
+    cats.unshift(name)
+}
+
+const destructivelyRemoveLastCat = () => {
+    cats.pop()
+}
+
+const destructivelyRemoveFirstCat = () => {
+    cats.shift()
+}
+
+const appendCat = (name) => {
+    return [...cats, name]
+}
+
+const prependCat = (name) => {
+    return [name, ...cats]
+}
+
+const removeLastCat = (name) => {
+    return cats.slice(0, cats.length - 1)
+}
+
+const removeFirstCat = (name) => {
+    return cats.slice(1)
+}
